04845ca7d4d4d8eeae83a2232b1691b447ef02d3
ssebastianoo <sebastianogirotto05@gmail.com>
2020-08-25 14:56:09 +0200

    init

---
+++ space.py
@@ -0,0 +1,52 @@
+import platform, os, random, time
+from assets import shuttle
+
+if platform.system() == "Windows":
+    cmd = "cls"
+
+else: 
+    cmd = "clear"
+
+space = 0
+reverse = False
+
+while True:
+
+    if space >= 15:
+        reverse = True 
+
+    elif space <= 0:
+        reverse = False
+
+    if reverse:
+        space -= 1
+
+    else:
+        space += 1
+
+    spaces = "\n" * space
+
+    spaces_down = 20 - space
+    spaces_down = "\n" * spaces_down 
+    
+    res = f"""
+{spaces}
+   __
+   \ \_____ {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])}           
+###[==_____>
+   /_/       {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} 
+{spaces_down}
+"""
+
+    result = []
+
+    for line in res.splitlines():
+        if len(line) != 0:
+            result.append(line)
+
+        else:
+            result.append(f"{' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} {' ' * random.choice(range(0, 10))} ----- {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} {' ' * random.choice(range(0, 10))} {random.choice(['-----', '-----', '-----', '•'])} ") 
+    
+    os.system(cmd)
+    print("\n".join(result))
+    time.sleep(0.3)
